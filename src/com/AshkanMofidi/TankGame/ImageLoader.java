2af9e9472d5c2030913affd71f87d87c29fc8f5f
Ashkan Mofidi <ashkan.mofidi@gmail.com>
2018-11-15 16:52:33 -0800

    update

--- src/com/AshkanMofidi/TankGame/ImageLoader.java
+++
@@ -1,4 +0,0 @@
-package com.AshkanMofidi.TankGame;
-
-public class ImageLoader {
-}
---
+++ src/com/AshkanMofidi/TankGame/gfx/ImageLoader.java
@@ -0,0 +1,37 @@
+package com.AshkanMofidi.TankGame.gfx;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/*
+    This ImageLoader class just simply load our images for us
+ */
+public class ImageLoader {
+    /*
+        Java stores images in BufferedImage objects
+        To load image we define a static method so that we can get access to it from everywhere
+        Its going to return a BufferedImage object, which is an object that has the image in it
+        this methods gets a String path, which is the location of our target image, as its parameter
+     */
+    public static BufferedImage loadImage(String path){
+
+        /*
+            So lets return our BufferedImage or load an Image and then return it here
+         */
+        try {
+            return ImageIO.read(ImageLoader.class.getResource(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            /*
+                Because if we can't load our image we do not want run our game
+             */
+            System.exit(1);
+        }
+        /*
+            We return null to just get rid of all java errors
+         */
+        return null;
+    }
+
+}
